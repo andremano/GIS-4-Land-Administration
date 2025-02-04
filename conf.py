b403f3495554db07754c591d965293de82c48396
Andre Mano <andre.s.mano@gmail.com>
2025-02-04 16:53:13 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
+sphinx_outputdir = "_build/html"
 
 # -- Project information -----------------------------------------------------
 
