836685aaace104e5d3a0bb10a38a41434054a8e1
Andre Mano <andre.s.mano@gmail.com>
2025-02-05 09:36:19 +0100

    a

--- conf.py
+++ conf.py
@@ -9,9 +9,8 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
-# import sys
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
 sphinx_outputdir = "_build/html"
